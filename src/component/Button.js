0e34cbfa0caf6b0a09cd5cb3a8b88821c7c04b4a
Dhegyaino <dtaiwo45@gmail.com>
2022-02-24 14:24:14 +0100

    Delete src directory

--- src/component/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({onClick, text, value}) => {
-  return (
-      <button onClick={onClick} value = {value}>{text.toUpperCase()}</button>
-  )
-};
-
-export default Button;
